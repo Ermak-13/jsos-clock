96baf2f2279453248fa1fdefd691c3c5f25d9701
Maksim Yermalovich <yermalovich.m@gmail.com>
2016-09-12 23:00:37 +0300

    test cache cdn.rawgit.com

--- module.js
+++ module.js
@@ -108,6 +108,7 @@ var Widget = require('./widget'),
     Shortcut = require('./shortcut'),
     locales = require('./locales');
 
+
 global.I18n.registryDict(locales);
 OS.installModule('Clock', {
   Widget: Widget,
